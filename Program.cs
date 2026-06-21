028d210b8b7d0e01c413af1d8035d9ab320a7228
comet <132081997+cometwarrior0@users.noreply.github.com>
2026-06-21 16:05:32 +0300

    cakisma cozuldu

--- Program.cs
+++ Program.cs
@@ -30,4 +30,22 @@ public class Program
         }
         return girilenSayi;
     }
+    public static void Secenekler()
+    {
+        Console.WriteLine("=================================================");
+        Console.WriteLine("          SAYI TAHMİN OYUNUNA HOŞ GELDİNİZ       ");
+        Console.WriteLine("=================================================");
+        Console.WriteLine("Kurallar:");
+        Console.WriteLine("- Bilgisayar 1 ile 100 arasında bir sayı tutar.");
+        Console.WriteLine("- Yaptığınız tahmine göre bilgisayar sizi yönlendirir.");
+        Console.WriteLine("- En az denemede sayıyı bulmaya çalışın!");
+        Console.WriteLine("-------------------------------------------------\n");
+    }
+
+    static int RastgeleSayiUret(int min, int max)
+    {
+        Random rastgele = new Random();
+        // max değere 1 ekliyoruz çünkü Next metodunda üst sınır dahil değildir.
+        return rastgele.Next(min, max + 1);
+    }
 }
