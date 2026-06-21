206038a05d684a918c1916d08a769f3065de3bf3
Temiralp <rtemir1881@gmail.com>
2026-06-21 15:29:01 +0300

    secenekler eklendi

--- Program.cs
+++ Program.cs
@@ -12,3 +12,20 @@ public static void Main(){}
 
 
 }
+
+
+public static void Secenekler(){
+
+
+            Console.WriteLine("=================================================");
+            Console.WriteLine("          SAYI TAHMİN OYUNUNA HOŞ GELDİNİZ       ");
+            Console.WriteLine("=================================================");
+            Console.WriteLine("Kurallar:");
+            Console.WriteLine("- Bilgisayar 1 ile 100 arasında bir sayı tutar.");
+            Console.WriteLine("- Yaptığınız tahmine göre bilgisayar sizi yönlendirir.");
+            Console.WriteLine("- En az denemede sayıyı bulmaya çalışın!");
+            Console.WriteLine("-------------------------------------------------\n");
+
+
+
+}
